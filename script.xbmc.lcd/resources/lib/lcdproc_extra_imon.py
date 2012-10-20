37096d6c25d59d0f113ee363fe92dce37f6c6059
Daniel Scheller <d.scheller@gmx.net>
2012-10-20 14:23:31 +0200

    iMON: LCDproc "returners", progress bar calculation

--- script.xbmc.lcd/resources/lib/lcdproc_extra_imon.py
+++ script.xbmc.lcd/resources/lib/lcdproc_extra_imon.py
@@ -28,6 +28,7 @@ __settings__ = sys.modules[ "__main__" ].__settings__
 __cwd__ = sys.modules[ "__main__" ].__cwd__
 __icon__ = sys.modules[ "__main__" ].__icon__
 
+from lcdproc import *
 from lcdbase import LCD_EXTRAICONS
 from extraicons import *
 
@@ -36,8 +37,49 @@ def log(loglevel, msg):
   
 class IMON_ICONS:
   ICON_SPINDISC = 0
+  BARS = 29
 
 class LCDproc_extra_imon():
   def __init__(self):
-    pass
-  
+    self.m_iOutputValueOldIcons = 1
+    self.m_iOutputValueOldBars = 1
+    self.m_iOutputValueIcons = 0
+    self.m_iOutputValueBars = 0
+
+  def __SetBar(self, barnum, value):
+    if barnum == 1:
+      bitmask = 0x00000FC0
+      bitshift = 6
+    elif barnum == 2:
+      bitmask = 0x00FC0000
+      bitshift = 18
+    elif barnum == 3:
+      bitmask = 0x0000003F
+      bitshift = 0
+    elif barnum == 4:
+      bitmask = 0x0003F000
+      bitshift = 12
+    else:
+      return self.m_iOutputValueProgress
+
+    self.m_iOutputValueBars = (self.m_iOutputValueBars &~ bitmask)
+    self.m_iOutputValueBars |= (int(32 * (value / 100)) << bitshift) & bitmask
+    self.m_iOutputValueBars |= 1 << IMON_ICONS.BARS
+
+  def SetOutputIcons(self):
+    ret = ""
+
+    if self.m_iOutputValueIcons != self.m_iOutputValueOldIcons:
+      self.m_iOutputValueOldIcons = self.m_iOutputValueIcons
+      ret += "output %d\n" % (self.m_iOutputValueIcons)
+
+    return ret
+
+  def SetOutputBars(self):
+    ret = ""
+
+    if self.m_iOutputValueBars != self.m_iOutputValueOldBars:
+      self.m_iOutputValueOldBars = self.m_iOutputValueBars
+      ret += "output %d\n" % (self.m_iOutputValueBars)
+
+    return ret
