7e5b78048c9e4e6dc42a83579e6a2d07db5e4c7f
Daniel Scheller <d.scheller@gmx.net>
2012-10-20 16:23:18 +0200

    Bitmasks for icon setting and clearing

--- script.xbmc.lcd/resources/lib/lcdproc_extra_imon.py
+++ script.xbmc.lcd/resources/lib/lcdproc_extra_imon.py
@@ -35,9 +35,53 @@ from extraicons import *
 def log(loglevel, msg):
   xbmc.log("### [%s] - %s" % (__scriptname__,msg,), level=loglevel) 
   
+# extra icon bitmasks
 class IMON_ICONS:
-  ICON_SPINDISC = 0
-  BARS = 29
+  ICON_SPINDISC        = 0x01 << 0
+  ICON_TOP_MUSIC       = 0x01 << 1
+  ICON_TOP_MOVIE       = 0x01 << 2
+  ICON_TOP_PHOTO       = (0x01 << 1) | (0x01 << 2)
+  ICON_TOP_CDDVD       = 0x01 << 3
+  ICON_TOP_TV          = (0x01 << 1) | (0x01 << 3)
+  ICON_TOP_WEBCASTING  = (0x01 << 2) | (0x01 << 3)
+  ICON_TOP_NEWSWEATHER = (0x01 << 1) | (0x01 << 2) | (0x01 << 3)
+  ICON_CH_2_0          = 0x01 << 4
+  ICON_CH_5_1          = 0x01 << 5
+  ICON_CH_7_1          = (0x01 << 4) | (0x01 << 5)
+  ICON_SPDIF           = 0x01 << 6
+  ICON_OUT_SRC         = 0x01 << 7
+  ICON_OUT_FIT         = 0x01 << 8
+  ICON_OUT_SD          = 0x01 << 9
+  ICON_OUT_HDTV        = 0x01 << 10
+  ICON_SCR1            = 0x01 << 11
+  ICON_SCR2            = 0x01 << 12
+  ICON_ACODEC_MP3      = 0x01 << 13
+  ICON_ACODEC_OGG      = 0x01 << 14
+  ICON_ACODEC_AWMA     = (0x01 << 13) | (0x01 << 14)
+  ICON_ACODEC_WAV      = 0x01 << 15
+  ICON_ACODEC_MPEG     = 0x01 << 16
+  ICON_ACODEC_AC3      = 0x01 << 17
+  ICON_ACODEC_DTS      = (0x01 << 16) | (0x01 << 17)
+  ICON_ACODEC_VWMA     = 0x01 << 18
+  ICON_VCODEC_MPEG     = 0x01 << 19
+  ICON_VCODEC_DIVX     = 0x01 << 20
+  ICON_VCODEC_XVID     = (0x01 << 19) | (0x01 << 20)
+  ICON_VCODEC_WMV      = 0x01 << 21
+  ICON_VOLUME          = 0x01 << 22
+  ICON_TIME            = 0x01 << 23
+  ICON_ALARM           = 0x01 << 24
+  ICON_REC             = 0x01 << 25
+  ICON_REPEAT          = 0x01 << 26
+  ICON_SHUFFLE         = 0x01 << 27
+  BARS                 = 0x01 << 28 # additionally needs bar values in other bits
+  ICON_DISC_IN         = 0x01 << 29
+
+  # clear masks
+  ICON_CLEAR_TOPROW    = 0xffffffff &~ ((0x01 << 1) | (0x01 << 2) | (0x01 << 3))
+  ICON_CLEAR_CHANNELS  = 0xffffffff &~ ((0x01 << 4) | (0x01 << 5))
+  ICON_CLEAR_BR        = 0xffffffff &~ ((0x01 << 13) | (0x01 << 14) | (0x01 << 15))
+  ICON_CLEAR_BM        = 0xffffffff &~ ((0x01 << 16) | (0x01 << 17) | (0x01 << 18))
+  ICON_CLEAR_BL        = 0xffffffff &~ ((0x01 << 19) | (0x01 << 20) | (0x01 << 21))
 
 class LCDproc_extra_imon():
   def __init__(self):
